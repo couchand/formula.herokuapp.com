fc53c6a2c0dfb0643a232246fdde4897c18f9bc0
Andrew Couch <andy@couchand.com>
2013-04-17 13:04:15 -0400

    test: get rid of wrapper function

--- test/parser.js
+++ test/parser.js
@@ -3,72 +3,68 @@
 var p = require('../dst/parser');
 var assert = require('assert');
 
-function assertEqual( a, b ) {
-    assert.equal( a, b );
-}
-
 it('should parse various things', function() {
 
-assertEqual( '5', p.parse('   5   ') );
+assert.equal( '5', p.parse('   5   ') );
 
 var decimal = p.parse('3.2');
-assertEqual( 'decimal', decimal.expression );
-assertEqual( '3', decimal.whole );
-assertEqual( '2', decimal.part );
+assert.equal( 'decimal', decimal.expression );
+assert.equal( '3', decimal.whole );
+assert.equal( '2', decimal.part );
 
 var str = p.parse('"foobar"');
-assertEqual( 'string', str.expression );
-assertEqual( 'foobar', str.string );
+assert.equal( 'string', str.expression );
+assert.equal( 'foobar', str.string );
 
 var sstr = p.parse("'foobar'");
-assertEqual( 'string', sstr.expression );
-assertEqual( 'foobar', sstr.string );
+assert.equal( 'string', sstr.expression );
+assert.equal( 'foobar', sstr.string );
 
 var today = p.parse(' today  (  ) ');
-assertEqual( 'today', today.function );
-assertEqual( 0, today.parameters.length );
+assert.equal( 'today', today.function );
+assert.equal( 0, today.parameters.length );
 
-assertEqual( '5', p.parse(' (  5  )  ').formula );
+assert.equal( '5', p.parse(' (  5  )  ').formula );
 
 var sum = p.parse('   sum (  3 ,  1 ,  8  ) ');
-assertEqual( 'function', sum.expression );
-assertEqual( 3, sum.parameters.length );
-assertEqual( '3', sum.parameters[0] );
-assertEqual( '1', sum.parameters[1] );
-assertEqual( '8', sum.parameters[2] );
+assert.equal( 'function', sum.expression );
+assert.equal( 3, sum.parameters.length );
+assert.equal( '3', sum.parameters[0] );
+assert.equal( '1', sum.parameters[1] );
+assert.equal( '8', sum.parameters[2] );
 
 var inverse = p.parse('   not  ( today \n\n(   )\n)\n');
-assertEqual( 'function', inverse.expression );
-assertEqual( 'not', inverse.function );
-assertEqual( 1, inverse.parameters.length );
-assertEqual( 'function', inverse.parameters[0].expression );
-assertEqual( 'today', inverse.parameters[0].function );
+assert.equal( 'function', inverse.expression );
+assert.equal( 'not', inverse.function );
+assert.equal( 1, inverse.parameters.length );
+assert.equal( 'function', inverse.parameters[0].expression );
+assert.equal( 'today', inverse.parameters[0].function );
 
 var field = p.parse('   relationship__r  . field__c ');
-assertEqual( 'reference', field.expression );
-assertEqual( 'relationship__r', field.name[0] );
-assertEqual( 'field__c', field.name[1] );
+assert.equal( 'reference', field.expression );
+assert.equal( 'relationship__r', field.name[0] );
+assert.equal( 'field__c', field.name[1] );
 
-assertEqual( '$User', p.parse('$User.Name').name[0] );
+assert.equal( '$User', p.parse('$User.Name').name[0] );
 
 var expr = p.parse('2+\n7*5');
-assertEqual( 'add', expr.expression );
-assertEqual( '2', expr.left );
-assertEqual( 'multiply', expr.right.expression );
-assertEqual( '7', expr.right.left );
-assertEqual( '5', expr.right.right );
+assert.equal( 'add', expr.expression );
+assert.equal( '2', expr.left );
+assert.equal( 'multiply', expr.right.expression );
+assert.equal( '7', expr.right.left );
+assert.equal( '5', expr.right.right );
 
 var order = p.parse('1<2||3&&4+5*6^7');
-assertEqual( 'comparison', order.expression );
-assertEqual( 'disjunction', order.right.expression );
-assertEqual( 'conjunction', order.right.right.expression );
-assertEqual( 'add', order.right.right.right.expression );
-assertEqual( 'multiply', order.right.right.right.right.expression );
-assertEqual( 'exponent', order.right.right.right.right.right.expression );
+assert.equal( 'comparison', order.expression );
+assert.equal( 'disjunction', order.right.expression );
+assert.equal( 'conjunction', order.right.right.expression );
+assert.equal( 'add', order.right.right.right.expression );
+assert.equal( 'multiply', order.right.right.right.right.expression );
+assert.equal( 'exponent', order.right.right.right.right.right.expression );
 
 var nest = p.parse('2+today()');
-assertEqual( 'add', nest.expression );
-assertEqual( '2', nest.left );
-assertEqual( 'today', nest.right.function );
+assert.equal( 'add', nest.expression );
+assert.equal( '2', nest.left );
+assert.equal( 'today', nest.right.function );
 
 });
